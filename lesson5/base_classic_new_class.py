2363c46e70aa1f11476fb4f015e2a3f222b23b72
hank <zhanghao_87@aliyun.com>
2016-11-21 17:39:31 +0800

    nothing

---
+++ lesson5/base_classic_new_class.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+class A:
+    #classic class
+    """this is class A"""
+    pass
+    __slots__=('x','y')
+    def test(self):
+        # classic class test
+        """this is A.test()"""
+        print "A class"
+
+class B(object):
+    #new class
+    """this is class B"""
+    __slots__=('x','y')
+    pass
+    def test(self):
+        # new class test
+        """this is B.test()"""
+        print "B class"
+
+        
+if __name__ == '__main__':
+    a=A()
+    b=B()
+    print dir(a)
+    print dir(b)
+
+    a.x=1
+    a.y=2
+    b.x=1
+    print dir(a)
+    print dir(b)
+    # help(a)
+    # help(b)
